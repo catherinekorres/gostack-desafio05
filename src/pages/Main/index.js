6f166db48f0494c2e63e86254001dd1cef93c135
Catherine Korres <cathy.korres@gmail.com>
2019-10-29 11:46:23 -0300

    catch input error

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -12,6 +12,7 @@ export default class Main extends Component {
     newRepo: '',
     repositories: [],
     loading: false,
+    inputError: false,
   };
 
   // Loading data from localStorage
@@ -43,23 +44,31 @@ export default class Main extends Component {
 
     const { newRepo, repositories } = this.state;
 
-    const response = await api.get(`/repos/${newRepo}`);
+    try {
+      const response = await api.get(`/repos/${newRepo}`);
 
-    const data = {
-      name: response.data.full_name,
-    };
+      const data = {
+        name: response.data.full_name,
+      };
 
-    this.setState({
-      repositories: [...repositories, data],
-      newRepo: '',
-      loading: false,
-    });
+      this.setState({
+        repositories: [...repositories, data],
+        newRepo: '',
+        loading: false,
+        inputError: false,
+      });
+    } catch (error) {
+      this.setState({
+        loading: false,
+        inputError: true,
+      });
 
-    console.log(repositories);
+      console.log(error);
+    }
   };
 
   render() {
-    const { newRepo, repositories, loading } = this.state;
+    const { newRepo, repositories, loading, inputError } = this.state;
 
     return (
       <Container>
@@ -68,7 +77,7 @@ export default class Main extends Component {
           Repositórios
         </h1>
 
-        <Form onSubmit={this.handleSubmit}>
+        <Form onSubmit={this.handleSubmit} inputError={inputError}>
           <input
             type="text"
             placeholder="Adicionar repositório"
